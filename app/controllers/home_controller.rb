408b4332812bc3995099384386842c5204ab3af1
Ben Williams <ben.williams@underdogfantasy.com>
2024-08-26 14:36:53 +0000

    clean up the "all users" section

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @user = current_user
-    @users = User.all
+    @user = User.find(current_user.id)
+    @users = User.all.map { |u| { id: u.id, email: u.email, is_following: u.followers.any? { |f| f.id == @user.id  } } }
   end
 end
