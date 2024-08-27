169b414c455c2ac127e2604ba65512631f90179c
Ben Williams <ben.williams@underdogfantasy.com>
2024-08-27 14:58:44 +0000

    replies working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,26 @@ class PostsController < ApplicationController
     @post_id = params[:id].to_i
     @post = Post.find(@post_id)
   end
+
+  def new
+    @post = Post.new
+    if params[:reply_to] != nil
+      @parentPost = Post.find(params[:reply_to])
+      @post.parent_id = params[:reply_to]
+    end
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :media, :parent_id)
+  end
 end
